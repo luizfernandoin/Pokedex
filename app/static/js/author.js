ad8197f49c67c2ecbeaf62624f0d1cb1ccf19bde
Jose Anderson Arruda <ferrazarrudaanderson@gmail.com>
2023-12-03 09:35:16 -0300

    feat: adicionando mudança de cursor para pointer

--- app/static/js/author.js
+++ app/static/js/author.js
@@ -1,7 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
+    const authors = document.querySelectorAll(".container-author");
     const authorJose = document.querySelector("#author-jose");
     const authorLuiz = document.querySelector("#author-luiz");
 
+    authors.forEach((author) => {
+        author.addEventListener("mouseover", () => {
+            author.style.cursor = "pointer";
+        });    
+    });
+    
     authorJose.addEventListener("click", () => {
         window.open("https://jandersonarruda.github.io/", "_blank");
     });
