788d6f7dc7912e7ec7c8337264d09cc54e9eded4
Luiz Fernando <luizfernandoinfo100@gmail.com>
2023-12-04 12:46:13 -0300

    feat: adicionando função para buscar informações de pokemon

--- app/static/js/pokeapi.js
+++ app/static/js/pokeapi.js
@@ -6,8 +6,17 @@ async function getAllPokemons() {
     const { results } = data;
 
     results.map(async (pokemon) => {
-
+        
     })
 }
+
+async function getMoreInfo(url) {
+    const response = await fetch(url);
+    const { name, abilities, types, species, height, weight, stats } = await response.json();
+
+    return {
+        name, abilities, types, species, height, weight, stats
+    }
+} 
 
 getAllPokemons();
